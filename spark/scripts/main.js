dc33e98c4ebe3787826947f27263bbb1551332ae
Eric1015 <love1dbsb77@hotmail.com>
2020-06-08 22:24:37 -0700

    Made it able to place an object with touch

--- spark/scripts/main.js
+++
@@ -1,9 +0,0 @@
-// Load in the required modules
-const Materials = require('Materials');
-const Scene = require('Scene');
-const Diagnostics = require('Diagnostics');
-const TouchGestures = require('TouchGestures');
-
-//export touch module
-const Touch = require('./touch.js');
-Touch.setDiamond();
