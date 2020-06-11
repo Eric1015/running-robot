f0df35510c44c47205ba6cb43508b336e84a2239
Yu Nishimura <yunishimura0716@gmail.com>
2020-06-11 00:39:59 -0700

    make UI to select 3d obj

--- src/optionUI.js
+++ src/optionUI.js
@@ -5,6 +5,7 @@ const Diagnostics = require('Diagnostics');
 const TouchGestures = require('TouchGestures');
 const NativeUI = require('NativeUI');
 const Textures = require('Textures');
+const CameraInfo = require('CameraInfo');
 
 function scale(object){
     object.transform.scaleX = 8;
@@ -12,7 +13,14 @@ function scale(object){
     object.transform.scaleZ = 8;
 
     object.transform.z = -0.5;
-    object.transform.y = 0.2;
+    object.transform.y = 0.3;
+}
+
+function captureHidden(planes, buttons, hiddenplane){
+    for(let i = 0; i < planes.length; i++){
+        planes[i].hidden = hiddenplane;
+        buttons[i].hidden = hiddenplane;
+    }
 }
 
 
@@ -20,31 +28,64 @@ function scale(object){
 function controlUI(){
     Promise.all([
         Scene.root.findByPath('**/canvas0/plane*'),
-        Scene.root.findByPath('planeTracker0/*'),
+        Scene.root.findByPath('planeTracker0/*')
     ]).then(function(results){
         const planes = results[0];
-        Diagnostics.log(planes[0].getMaterial());
         const objects = results[1];
+        const buttons = [];
+        for(let i = 0; i < planes.length; i++){
+            buttons.push(planes[i].find('button' + i));
+        }
 
         Diagnostics.log('Start');
 
-        // Diagnostics.log(objects[0]);
+        // set default value of visibility of objects
         objects[0].cameraVisibility.forBackCamera = false;
         objects[1].cameraVisibility.forBackCamera = false;
         objects[2].cameraVisibility.forBackCamera = false;
-        // Diagnostics.log("!");
+
+        buttons[0].cameraVisibility.forBackCamera = false;
+        buttons[1].cameraVisibility.forBackCamera = false;
+        buttons[2].cameraVisibility.forBackCamera = false;
+        // set the default scale value for each objects
         scale(objects[0]);
         scale(objects[1]);
         scale(objects[2]);
 
+        // Hide the plane when capturing a photo or recording a video
+        // Store references to the photo capture and video recording boolean signals
+        const isCapturingPhoto = CameraInfo.isCapturingPhoto;
+        const isRecordingVideo = CameraInfo.isRecordingVideo;
+        
+        // Create a boolean signal that returns true if either boolean sign
+        const hidePlane = isCapturingPhoto.or(isRecordingVideo);
+        captureHidden(planes, buttons, hidePlane);
+
+        //change the visibility by touhch
+        let oldIndex;
+
+
         for(let i = 0; i < objects.length; i++){
             TouchGestures.onTap(planes[i]).subscribe(function (gesture) {
                 // Diagnostics.log("Tap!")
+                // if(oldIndex === undefined){
+                //     objects[i].cameraVisibility.forBackCamera = true;
+                //     oldIndex = i;
+                // } else if (oldIndex !== i){
+                //     objects[oldIndex].cameraVisibility.forBackCamera = false;
+                //     objects[i].cameraVisibility.forBackCamera = true;
+                //     oldIndex = i;
+                // } else {
+                //     objects[oldIndex].cameraVisibility.forBackCamera = false;
+                //     oldIndex = i;
+                // }
                 for(let j = 0; j < objects.length; j++){
-                    if(j === i){
-                        objects[i].cameraVisibility.forBackCamera = true;        
+                    if(i === j){
+                        objects[i].cameraVisibility.forBackCamera = true;
+                        buttons[i].cameraVisibility.forBackCamera = true;
                     } else {
                         objects[j].cameraVisibility.forBackCamera = false;
+                        buttons[j].cameraVisibility.forBackCamera = false;
                     }
                 }
             });
